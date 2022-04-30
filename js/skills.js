dbc2cd17ce0041b683dfec514e6a973d75778e67
Giudicelli Elias <giudicellielias@gmail.com>
2022-04-29 23:06:59 -0300

    Atualizações

--- js/skills.js
+++ js/skills.js
@@ -1,2 +1,31 @@
-var buttons = document.querySelectorAll('.div-skills ul h3');
+var buttons = document.querySelectorAll('.div-skills ul a');
+var atual = 0;
+var primeiro = document.getElementById('0');
+var section = document.getElementById('html');
+
+primeiro.classList.add('active');
+
+for(let i =0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', ()=>{
+        atual = buttons[i].id;
+        mudarButton();
+
+        if (section.className.endsWith('primeiraSection')){
+            removerPrimeiraSection();
+        }else{
+
+        }
+        
+    });
+}
+
+function mudarButton(){
+    document.querySelector('.active').classList.remove('active');
+    document.getElementById(atual).classList.add('active');
+}
+
+function removerPrimeiraSection(){
+    document.querySelector('.primeiraSection').classList.remove('primeiraSection');
+
+}
 
